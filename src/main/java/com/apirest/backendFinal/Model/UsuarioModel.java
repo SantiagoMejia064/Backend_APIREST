36a3ef9fbd142825c7c2274e69bab51e580aec8b
SantiagoMejia064 <mejiasantiago0604@gmail.com>
2025-11-14 16:57:57 -0500

    Primer model

---
+++ src/main/java/com/apirest/backendFinal/Model/UsuarioModel.java
@@ -0,0 +1,26 @@
+package com.apirest.backendFinal.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Usuario")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioModel {
+
+    @Id
+    private Integer idUsuario;
+    private String nombreCompleto;
+    private Integer edad;
+    private String ocupacion;
+    private String correoElectronico;
+    private Integer telefono;
+    private String rol;  // Asumimos que es un String, ya que el ENUM es solo en la DB
+}
+
